c9b943713af526487ce896577318afe28f2c7ba6
Tim Hannon <tim@cognitiveagency.ai>
2023-07-23 16:39:33 +1000

    write here your commit for example Jira Task Id: Test-23

--- coginstall.sh
+++ coginstall.sh
@@ -151,7 +151,7 @@ wget https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/
 sudo dpkg -i cuda-repo-ubuntu2004-11-8-local_11.8.0-520.61.05-1_amd64.deb
 sudo cp /var/cuda-repo-ubuntu2004-11-8-local/cuda-*-keyring.gpg /usr/share/keyrings/
 sudo apt-get update
-sudo apt-get -y install cud
+sudo apt-get -y install cuda
 
 # Setting up NVIDIA Toolkit for GPU-accelerated container support.
 print_message "Setting up NVIDIA Toolkit"
