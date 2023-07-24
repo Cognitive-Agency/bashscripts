56121db8ab166991cbdd592fdc729aae6a12e7ff
Tim Hannon <tim@cognitiveagency.ai>
2023-07-24 23:27:10 +1000

    Your commit message here

--- coginstall-docker.sh
+++ coginstall-docker.sh
@@ -3,7 +3,6 @@
 set -e  # Exit on any error
 
 # Downloading new zsh bash file and replacing the old one
-print_message "Downloading new zsh bash file and replacing the old one"
 curl -fsSL https://raw.githubusercontent.com/Cognitive-Agency/bashscripts/main/.zshrc -o ~/.zshrc
 
 # Declaring list of packages with their descriptions
@@ -11,7 +10,6 @@ apt update
 apt install -y git curl vim htop build-essential software-properties-common apt-transport-https ca-certificates gnupg-agent cmake gnupg nvtop screen git-lfs ffmpeg tree
 
 # Setting up Miniconda
-print_message "Setting up Miniconda"
 if [ ! -d "$HOME/anaconda3" ]; then
     cd /tmp
     curl -fsSL https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o Miniconda3-latest-Linux-x86_64.sh
@@ -30,6 +28,5 @@ fi
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
     echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >> ~/.zshrc
 
-print_message "Installation complete."
 
 
